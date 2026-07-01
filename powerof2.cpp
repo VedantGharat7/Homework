f4379d7619dfeaf8931ce4c7f548720baaef671e
VedantGharat7 <gharatvedant2@gmail.com>
2026-07-01 11:45:12 +0530

    homework lec 7 solved on my own method 1

---
+++ powerof2.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+using namespace std;
+bool number(int n){
+
+    int pow=1;
+if (n==1)
+{
+    return 1;
+}
+
+    for ( int i = 1; i <=n; i++)
+    {   pow*=2;
+        if (n-pow==0)
+        {
+            return 1;
+        }
+        
+    }return 0;
+    
+
+
+
+
+
+
+}
+
+int main(){
+
+if (number(1))
+{
+    cout << "its a power of 2";
+}else{
+    cout << "its not a power of 2";
+}
+
+
+
+
+    return 0;
+}
