ee3887c3a8259de55690dbc9b7f25d17a5bb4e1b
VedantGharat7 <gharatvedant2@gmail.com>
2026-07-08 08:38:04 +0530

    made the homework on my own ts time

--- lecture 8/lvip.cpp
+++ lecture 8/lvip.cpp
@@ -5,19 +5,23 @@ using namespace std;
 int main(){
 int smallest,largest,size,store;
 size = 5;
- int nums [] = {22,1,19,5,2};
+ int nums [size] = {22,10,19,5,22};
 store =0;
 smallest = INT_MAX;
 largest = INT_MIN;
 for ( int  i = 0; i < size; i++)
 {
-    if (nums[i] <smallest)
-    {
-        smallest = nums[i];
-        store = i; // stores the index value
-    }
+   if (nums[i] < smallest)
+   {
+    smallest = nums[i];
+    store = i;
+   }
+   
     
-} cout << "positional value =" << store;
+}
+cout << smallest << endl;
+ 
+cout << "positional value =" << store;
 
 
 
