0b3214181383960ef7e6232c94a5d27c25863c36
VedantGharat7 <gharatvedant2@gmail.com>
2026-07-10 12:15:56 +0530

    made a vector version of reversearray YATTA!!!

---
+++ lecture 9/reversevector.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void reverse(vector<int> & vec){
+   int start =0; int end = vec.size()-1;
+    while (start<end) // will stop after both of em meet in middle
+    {
+        swap(vec[start],vec[end]);
+        start++;  end--;
+    }
+    
+}
+
+
+
+
+
+int main(){
+
+vector<int>vec = {2,5,6,8,9,10};
+
+reverse(vec);
+cout << "The reverse array = ";
+for ( int val:vec)
+{
+    cout << val << " ";
+}
+
+
+
+    return 0;
+}
